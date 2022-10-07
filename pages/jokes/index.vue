e0eccd55ccf310647b4a96c2b4a07691c9f5657d
Parsa361 <parsafarahany821@gmail.com>
2022-10-07 13:40:10 +0330

    change asyncData with created, add searchText func

--- pages/jokes/index.vue
+++ pages/jokes/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h2 class="text-center mt-5">Welcome to the dad jokes page</h2>
+    <search-jokes class="mt-5" v-on:search-text="searchText" />
     <hr />
     <Joke
       v-for="joke in jokes"
@@ -15,9 +16,15 @@
 <script>
 import axios from "axios";
 import Joke from "~/components/Joke.vue";
+import SearchJokes from "~/components/SearchJokes.vue";
 
 export default {
-  async asyncData() {
+  data() {
+    return {
+      jokes: [],
+    };
+  },
+  async created() {
     const config = {
       headers: {
         Accept: "application/json",
@@ -25,8 +32,7 @@ export default {
     };
     try {
       let res = await axios.get("https://icanhazdadjoke.com/search", config);
-      let jokes = res.data.results;
-      return { jokes };
+      this.jokes = res.data.results;
     } catch (error) {
       console.log(error);
     }
@@ -43,8 +49,25 @@ export default {
       ],
     };
   },
+  methods: {
+    async searchText(text) {
+      const config = {
+        headers: {
+          Accept: "application/json",
+        },
+      };
+      try {
+        console.log(text);
+        let res = await axios.get(`https://icanhazdadjoke.com/search?term=${text}`, config);
+        this.jokes = res.data.results;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
   components: {
     Joke,
+    SearchJokes,
   },
 };
 </script>
