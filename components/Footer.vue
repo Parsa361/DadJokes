39755f61cb02f693fd60aeafc95566e9dbbc67ee
Parsa361 <parsafarahany821@gmail.com>
2022-10-07 13:38:09 +0330

    Fix footer position

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,11 +1,18 @@
 <template>
-    <div>
-        
-    </div>
+  <div class="possition bg-light" style="height: 100px;">
+    <h2 class="text-center p-5">Footer</h2>
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
+
+<style scoped>
+.possition {
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  left: 0;
+}
+</style>
